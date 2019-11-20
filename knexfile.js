854921a95b47ef08301cdda82740beb62f1a4cae
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-20 14:40:41 -0600

    Fixed another readme error and added test db

--- knexfile.js
+++ knexfile.js
@@ -21,21 +21,21 @@ module.exports = {
     },
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/testing.sqlite3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
+  },
 
   production: {
     client: 'pg',
